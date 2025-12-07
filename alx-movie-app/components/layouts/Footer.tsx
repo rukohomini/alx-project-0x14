5dc1c497e681bdcb308a910e72de0cb4ba96c1e9
Rukoh Omini <rukohbenjamin@gmail.com>
2025-12-07 23:04:14 +0000

    Add Footer component

--- alx-movie-app/components/layouts/Footer.tsx
+++ alx-movie-app/components/layouts/Footer.tsx
@@ -2,11 +2,12 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="p-4 bg-gray-800 text-white text-center">
+    <footer className="p-4 bg-black text-white text-center">
       Footer Component
     </footer>
   );
 };
 
 export default Footer;
+
 
