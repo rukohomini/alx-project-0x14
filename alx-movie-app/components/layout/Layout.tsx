479adacec605f97f994586e3378d6cc47383ad3b
Rukoh Omini <rukohbenjamin@gmail.com>
2025-12-07 22:21:31 +0000

    Fix folder structure: Header, Layout, Button in layout; Footer in layouts

--- alx-movie-app/components/layout/Layout.tsx
+++ alx-movie-app/components/layout/Layout.tsx
@@ -1,7 +1,6 @@
 import React, { ReactNode } from "react";
-import Header from "../layout/Header";
-
-import Footer from "./Footer";
+import Header from "./Header";
+import Footer from "../layouts/Footer";
 
 interface Props { children: ReactNode }
 
