a00c09623e3965de525b7363404365fdad9ca6cb
Rukoh Omini <rukohbenjamin@gmail.com>
2025-12-08 00:00:41 +0000

    Add movies page, MovieCard, Loading, interfaces, and API for CineSeek

--- alx-movie-app/components/commons/MovieCard.tsx
+++ alx-movie-app/components/commons/MovieCard.tsx
@@ -1,18 +1,18 @@
-import React from "react";
-interface MovieCardProps {
-  title: string;
-  poster: string;
-  releaseDate: string;
+import { MovieProps } from "@/interfaces"
+import Image from "next/image"
+
+const MovieCard: React.FC<MovieProps> = ({ title, posterImage, releaseYear }) => {
+  return (
+    <div className="h-[563px]">
+      <div>
+        <Image className="h-[430px] w-full rounded-md hover:cursor-pointer" src={posterImage} width={100} height={100} alt={title} />
+      </div>
+      <div className="flex justify-between py-4">
+        <p className="text-xl font-bold">{title}</p>
+        <p className="text-xl text-[#E2D609]">{releaseYear}</p>
+      </div>
+    </div>
+  )
 }
 
-const MovieCard: React.FC<MovieCardProps> = ({ title, poster, releaseDate }) => {
-  return (
-    <div className="border rounded shadow hover:shadow-lg transition p-2">
-      <img src={poster} alt={title} className="w-full h-64 object-cover rounded" />
-      <h3 className="mt-2 font-bold">{title}</h3>
-      <p className="text-gray-500">{releaseDate}</p>
-    </div>
-  );
-};
-
-export default MovieCard;
+export default MovieCard
